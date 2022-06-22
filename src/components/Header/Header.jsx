622722729dffd78014c7cb0b7894b80349edddf9
Aditya <adityaprasadmohanty150@gmail.com>
2022-06-23 01:45:44 +0530

    Changes made

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,6 +18,7 @@ const Header = () => {
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
+              <InputBase placeholder="Search" classes={{ root: classes.inputRoot, input: classes.inputInput }}/>
             </div>
           </Autocomplete>
         </Box>
