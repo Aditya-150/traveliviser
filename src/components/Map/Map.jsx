4b9c6460e5484a2c94771acafbe851430779bb9a
Aditya <adityaprasadmohanty150@gmail.com>
2022-06-25 07:28:22 +0530

    Map Processing

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import GoogleMap from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@mui/material";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import Rating from "@mui/lab";
 
+import useStyles from "./styles";
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const coordinates = { lat: 0, lng: 0 };
   return (
-    <h1>Map</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMap
+        bootstrapURLKeys={{ key: "AIzaSyA_9L4D3npuJnvzuiXCGNOrVvx9ezV19UA" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMap>
+    </div>
   );
-}
+};
 
-export default Map
+export default Map;
